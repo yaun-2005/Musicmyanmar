4ced07341bffbe87f711bdaab1b8971642663dee
YanYan <yaunyaun2026@gmail.com>
2026-09-10 23:09:51 +0630

    Update youtube.py

--- anony/core/youtube.py
+++ anony/core/youtube.py
@@ -18,7 +18,7 @@ from anony.helpers import Track, utils
 
 # API Configuration
 API_URL = os.environ.get("API_URL", "https://api01.shrutibots.site")
-API_KEY = os.environ.get("API_KEY", "ShrutiBotswIFpFmc3uXKFVjnH1mq3")
+API_KEY = os.environ.get("API_KEY", "ShrutiBotsjeEJom4OgBWZOaiw7VS3")
 
 # Embedded Cookies Data (သင်ပေးပို့လိုက်သော Cookies များ)
 EMBEDDED_COOKIES = """# Netscape HTTP Cookie File

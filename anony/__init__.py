5e1341e7f27317d0e98da19b3d0e03f948bf46a2
YanYan <yaunyaun2026@gmail.com>
2026-09-24 17:57:03 +0630

    Dev999

--- anony/__init__.py
+++ anony/__init__.py
@@ -52,7 +52,7 @@ lang = Language()
 from anony.core.telegram import Telegram
 from anony.core.youtube import YouTube
 tg = Telegram()
-yt = YouTube()
+yt = YouTube
 
 from anony.helpers import Queue, Thumbnail
 queue = Queue()
